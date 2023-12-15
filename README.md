35d7dee15a600883a17435b1e6e302d1f4185f41
Samoina <samoina.w@gmail.com>
2023-12-15 08:30:50 +0300

    Change the order of my JS logic so that the player clicks come first then the checking for a win

--- README.md
+++ README.md
@@ -168,7 +168,47 @@ To check the columns, we use the same approach. We need to loop through
 II. A draw - in this case, the entire grid is filled with 'X' and 'O', and none of these are placed consecutively whether horizontally, vertically or diagonally.
 
 ```javascript
+let gameBoard = [
+	['1', '2', '3'],
+	['4', '5', '6'],
+	['7', '8', '9'],
+];
 
+const rows = 3;
+const columns = 3;
+const diagonalValues = [];
+
+//this would actually check for the rows equality
+for (let i = 0; i < rows; i++) {
+	const a = gameBoard[i][0];
+	const b = gameBoard[i][1];
+	const c = gameBoard[i][2];
+
+	console.log(a);
+
+	a === 1 && diagonalValues.push(a);
+
+	//in the first iteration, a=1, b=2, c=3
+	//in the second iteration, a=4, b=5, c=6
+	//in the third iteration, a=7, b=8, c=9
+	//so now we need to check, if a is not empty, and it is strictly equal to b and  is strictly equal to c, then that is a win.
+
+	//i now need to do another set of iteration where i get the columns, this would be the inner array we are looping over
+	for (let j = 0; j < columns; j++) {
+		const d = gameBoard[0][j];
+		const e = gameBoard[1][j];
+		const f = gameBoard[2][j];
+
+		//in the first iteration d=1, e=4 and f=7
+		//in the second iteration d=2, e=5 and f=8
+		//in the third iteration d=3, e=6 and f=9
+		//so the same logic applies. we need to check that d which is at the to of the column, is not empty, and then check it d is strictly equal to e and if e is strictly equal to f then that is also a win
+
+		//there is also a win if the diagonals are matching, but how do i get the diagonals during the iterations?
+	}
+}
+
+console.log(diagonalValues);
 ```
 
 ### Screenshot
--- scripts.js
+++ scripts.js
@@ -7,35 +7,6 @@ let gameBoard = [
 	['7', '8', '9'],
 ];
 
-const rows = 3;
-const columns = 3;
-
-//this would actually check for the rows equality
-for (let i = 0; i < rows; i++) {
-	const a = gameBoard[i][0];
-	const b = gameBoard[i][1];
-	const c = gameBoard[i][2];
-
-	//in the first iteration, a=1, b=2, c=3
-	//in the second iteration, a=4, b=5, c=6
-	//in the third iteration, a=7, b=8, c=9
-	//so now we need to check, if a is not empty, and it is strictly equal to b and  is strictly equal to c, then that is a win.
-
-	//i now need to do another set of iteration where i get the columns, this would be the inner array we are looping over
-	for (let j = 0; j < columns; j++) {
-		const d = gameBoard[0][j];
-		const e = gameBoard[1][j];
-		const f = gameBoard[2][j];
-
-		//in the first iteration d=1, e=4 and f=7
-		//in the second iteration d=2, e=5 and f=8
-		//in the third iteration d=3, e=6 and f=9
-		//so the same logic applies. we need to check that d which is at the to of the column, is not empty, and then check it d is strictly equal to e and if e is strictly equal to f then that is also a win
-
-		//there is also a win if the diagonals are matching, but how do i get the diagonals during the iterations?
-	}
-}
-
 //Get all the elements by their classname to give me an iterable HTMLCollection
 let gameElements = document.getElementsByClassName('game-square');
 
@@ -62,3 +33,50 @@ for (const element of gameElements) {
 		}
 	);
 }
+//create the logic to find the winning comination
+const rows = 3;
+const columns = 3;
+const diagonalValues = [];
+const diagonalValues2 = [];
+
+//this would actually check for the rows equality
+for (let i = 0; i < rows; i++) {
+	const a = gameBoard[i][0];
+	const b = gameBoard[i][1];
+	const c = gameBoard[i][2];
+
+	//check a win horizontally
+	if (a !== '' && a === b && b === c) {
+		console.log('Game Won!');
+	}
+
+	// get first diagonal
+	a === '1' && diagonalValues.push(a);
+	b === '5' && diagonalValues.push(b);
+	c === '9' && diagonalValues.push(c);
+}
+//i now need to do another set of iteration where i get the columns, this would be the inner array we are looping over
+for (let j = 0; j < columns; j++) {
+	const d = gameBoard[0][j];
+	const e = gameBoard[1][j];
+	const f = gameBoard[2][j];
+
+	//check column wins
+	if (d !== '' && d === e && e === f) {
+		console.log('Game Won!');
+	}
+
+	//second diagonal
+	if (f === '7' && j === 0) {
+		diagonalValues2.push(f);
+	}
+	if (e === '5' && j === 1) {
+		diagonalValues2.push(e);
+	}
+	if (d === '3' && j === 2) {
+		diagonalValues2.push(d);
+	}
+}
+
+console.log(diagonalValues);
+console.log(diagonalValues2);
